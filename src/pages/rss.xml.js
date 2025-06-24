a39b365e6e3a4c6c80157026ca9919a1f7def8e5
intergrav <devin@devins.page>
2025-06-24 17:15:01 -0400

    feat: content in rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,8 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
 export async function GET(context) {
 	const blog = await getCollection("blog");
@@ -8,10 +11,11 @@ export async function GET(context) {
 		description: "hi, i'm devin! :)",
 		site: context.site,
 		items: blog.map((post) => ({
-			title: post.data.title,
-			pubDate: post.data.pubDate,
-			description: post.data.description,
 			link: `/blog/${post.slug}/`,
+			content: sanitizeHtml(parser.render(post.body), {
+				allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+			}),
+			...post.data,
 		})),
 	});
 }
