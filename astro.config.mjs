ef25c52dd5acdd3b7d5be6622fc50d5568d7eb3f
intergrav <devin@devins.page>
2025-06-26 14:13:20 -0400

    feat: theming fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,15 @@
 import { defineConfig } from "astro/config";
-import { createCssVariablesTheme } from "shiki/core";
 import sitemap from "@astrojs/sitemap";
-
-const cssVariablesTheme = createCssVariablesTheme({
-	name: "css-variables",
-	variablePrefix: "--shiki-",
-});
 
 export default defineConfig({
 	site: "https://devins.page",
 	integrations: [sitemap()],
 	markdown: {
 		shikiConfig: {
-			theme: cssVariablesTheme,
+			themes: {
+				light: "catppuccin-latte",
+				dark: "catppuccin-mocha",
+			},
 		},
 	},
 });
